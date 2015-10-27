b00d62d9ee0713a55166f132700787e5fbd561d3
cthree-40 <clmalbon@gmail.com>
2015-10-27 18:44:32 -0400

    Removed superfluous test/ directory.

--- test/source.c/initarray.h
+++
@@ -1,7 +0,0 @@
-// File: initarray.h
-#ifndef initarray_h
-#define initarray_h
-
-void initarray(double *array, int dim);
-
-#endif
--- test/source/colib/newsif/sif_scalar.f
+++
@@ -1,8 +0,0 @@
-       subroutine sif_scalar()
-
-c
-c      purpose read/write the scalar data on a sifs file
-c
-       return
-       end
-
