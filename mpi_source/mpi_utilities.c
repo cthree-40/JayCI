fdb858f2f9f43156b73cf6ecb1caa6e2a689bde2
cthree-40 <clmalbon@gmail.com>
2020-05-06 18:27:44 -0400

    Moved mpi_global variables to mpi_utilities.h

--- mpi_source/mpi_utilities.c
+++ mpi_source/mpi_utilities.c
@@ -10,7 +10,7 @@
 #include "errorlib.h"
 #include "iminmax.h"
 #include "mpi_utilities.h"
-#include "pjayci_global.h"
+//#include "pjayci_global.h"
 #include <mpi.h>
 #include <ga.h>
 #include <macdecls.h>
